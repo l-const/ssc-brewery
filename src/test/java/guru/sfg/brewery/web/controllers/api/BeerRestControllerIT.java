e5f139d2d316ebdf547ae92e75a3bca56ddbb18f
l-const <const_l@outlook.com>
2022-10-10 00:50:23 +0300

    checkpoint 2 userDetailsService

---
+++ src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
@@ -0,0 +1,30 @@
+package guru.sfg.brewery.web.controllers.api;
+
+import guru.sfg.brewery.web.controllers.BaseIT;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class BeerRestControllerIT extends BaseIT {
+
+    @Test
+    void findBeers() throws Exception {
+        mockMvc.perform(get("/api/v1/beer/"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void findBeerById() throws Exception {
+        mockMvc.perform(get("/api/v1/beer/a805ff12-4818-11ed-b878-0242ac120002"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void findBeerByUpc() throws Exception {
+        mockMvc.perform(get("/api/v1/beerUpc/8044441222422"))
+                .andExpect(status().isOk());
+    }
+}
